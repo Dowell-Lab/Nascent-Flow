956b7b1ccc27e5ba7a430a812866223f3932c65b
Ignacio Tripodi <ignacio.tripodi@colorado.edu>
2018-07-28 13:56:58 -0600

    Customized the parser to the tools used with nascent transcription assays.

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -8,15 +8,15 @@ regexes = {
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
     'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
     'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
-    #'Trim Galore!': ['v_trim_galore.txt', r"version (\S+)"],
-    'Trimmmomatic': ['v_trimmomatic.txt', r"version (\S+)"],
     'Samtools': ['v_samtools.txt', r"samtools (\S+)"],
-    'Bowtie2': ['v_bowtie2.txt', r"version (\S+)"],
     'fastq-dump': ['v_fastq-dump.txt', r"fastq-dump : (\S+)"],
     'Bedtools': ['v_bedtools.txt', r"bedtools v(\S+)"],
     'IGV Tools': ['v_igv-tools.txt', r"IGV Version (\S+)"],
-    'MACS2': ['v_macs2.txt', r"macs2 (\S+)"]
-
+    'FastX Reverse Complement': ['v_fastx_reverse_complement.txt', r"FASTX Toolkit (\S+) by"],
+    'BBduk': ['v_bbduk.txt', r"(\S+)"],
+    'Hisat2': ['v_hisat2.txt', r"hisat2-align-s version (\S+)"],
+    'preseq': ['v_preseq.txt', r"(\S+)"],
+    'RseQC': ['v_rseqc.txt', r"(\S+)"],
 }
 results = OrderedDict()
 results['NCBI-Hackathons/ATACFlow'] = '<span style="color:#999999;\">N/A</span>'
